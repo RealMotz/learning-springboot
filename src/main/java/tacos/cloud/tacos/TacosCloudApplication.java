b5cb5fd192f579ad75fee3da9eb18f902a2dc490
Motta <carlos.motta.business@gmail.com>
2024-07-08 20:34:42 +0900

    first commit

---
+++ src/main/java/tacos/cloud/tacos/TacosCloudApplication.java
@@ -0,0 +1,12 @@
+package tacos.cloud.tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacosCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacosCloudApplication.class, args);
+	}
+}
