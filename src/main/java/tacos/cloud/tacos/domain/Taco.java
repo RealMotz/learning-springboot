6aca01e37ed0d8547280dbaa4136b701a3e6a644
Motta <carlos.motta.business@gmail.com>
2024-07-19 01:00:51 +0900

    Add design controller

---
+++ src/main/java/tacos/cloud/tacos/domain/Taco.java
@@ -0,0 +1,13 @@
+package tacos.cloud.tacos.domain;
+
+import java.util.List;
+import lombok.Data;
+ 
+@Data
+public class Taco {
+ 
+  private String name;
+  
+  private List<Ingredient> ingredients;
+ 
+}
