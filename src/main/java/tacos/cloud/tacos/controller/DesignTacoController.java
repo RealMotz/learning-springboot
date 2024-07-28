caa322a89147cef5f6132aa8fdbdc528ead961f7
Motta <carlos.motta.business@gmail.com>
2024-07-29 00:43:06 +0900

    Add order controller and view.
    Add form validations.

--- src/main/java/tacos/cloud/tacos/controller/DesignTacoController.java
+++ src/main/java/tacos/cloud/tacos/controller/DesignTacoController.java
@@ -5,11 +5,14 @@ import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import tacos.cloud.tacos.domain.Taco;
 import tacos.cloud.tacos.domain.TacoOrder;
@@ -56,6 +59,20 @@ public class DesignTacoController {
     @GetMapping
     public String showDesignForm() {
         return "design";
+    }
+
+    @PostMapping
+    public String processTaco(@Valid Taco taco, Errors errors,
+            @ModelAttribute TacoOrder tacoOrder) {
+        
+        if(errors.hasErrors()) {
+            return "design";
+        }
+
+        tacoOrder.addTaco(taco);
+        log.info("Processing taco: {}", taco);
+
+        return "redirect:/orders/current";
     }
 
     private Iterable<Ingredient> filterByType(
